685cd31aa37784a77f02a96fc176c4f58bc08084
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-02-28 16:59:55 +0000

    adding logs t help diagnose problem

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -1,6 +1,7 @@
 const User = require("./userModel");
 
 exports.addUser = async (req, res) => {
+  console.log(req.body);
   try {
     const newUser = await User.create(req.body);
     res.status(200).send({ user: newUser });
