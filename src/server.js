80da9e45b68997112d0b0bc9bea3af33150841c2
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-02-28 16:02:26 +0000

    changing port origin

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const movieRouter = require("./movies/movieRoutes");
 const app = express();
 const userRouter = require("./user/userRoutes");
 const cors = require("cors");
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 app.use(express.json()); //allows API to parse JSON
 app.use(cors());
