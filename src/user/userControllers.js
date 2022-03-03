d5a97793d032f2a85997e2408d249a0646df68de
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-03-03 13:25:51 +0000

    User document in db now has taskTally attribute

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -1,9 +1,10 @@
 const User = require("./userModel");
 
 exports.addUser = async (req, res) => {
-  console.log(req.body);
   try {
+    console.log("addUser", req.body);
     const newUser = await User.create(req.body);
+    console.log("newUser", newUser);
     res.status(200).send({ user: newUser });
   } catch (error) {
     console.log(error);
@@ -40,7 +41,7 @@ exports.getStoredHash = async (username) => {
 exports.giveToken = async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    console.log(user);
+    console.log("giveToken", user);
     res.status(200).send(user);
   } catch (error) {
     console.log(error);
